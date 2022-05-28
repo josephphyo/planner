400d4aaa7070729889e33de4094799afc8883568
Phyo Min Htun <phyominhtun1990@gmail.com>
2022-05-28 17:47:17 +0700

    modified

---
+++ convert/go.mod
@@ -0,0 +1,5 @@
+module github.com/josephphyo/planner/convert
+
+go 1.18
+
+require github.com/josephphyo/planner v0.0.0-20220528101110-220a1d81291a // indirect
--- convert/main.go
+++ convert/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/josephphyo/planner/dates"
+)
+
+func main() {
+	fmt.Print("Would you like to convert WeekToDay (or) DayToWeek \n")
+	fmt.Print("If you want to convert WeekToDay .. type wtd \n")
+	fmt.Print("If you want to convert DayToWeek .. type dtw \n")
+	for true {
+		fmt.Print("Your input wtd(or)dtw = ")
+
+		var input string
+		fmt.Scan(&input)
+
+		switch input {
+		case "wtd":
+			var week int
+			fmt.Print("How many days in week: ")
+			fmt.Scan(&week)
+			fmt.Println(dates.WeekToDays(week))
+		case "dtw":
+			var day int
+			fmt.Print("How many week in days: ")
+			fmt.Scan(&day)
+			fmt.Println(dates.DayToWeek(day))
+		case "quit":
+			return
+		}
+	}
 }
